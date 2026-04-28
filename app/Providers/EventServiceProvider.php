fd85a43eb606dcb7ff92e5fba55d46f087414e2e
Dario Becchio <dabek1986@gmail.com>
2026-04-28 12:14:45 +0200

    Create observer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,37 +2,40 @@
 
 namespace App\Providers;
 
+use App\Mail\WelcomeNewUser;
 use App\Models\User;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Mail;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        Verified::class => [],
     ];
 
-    /**
-     * Register any events for your application.
-     */
     public function boot(): void
     {
-         User::observe(UserObserver::class);
+        User::observe(UserObserver::class);
+
+        Event::listen(
+            Verified::class,
+            function (Verified $event) {
+                /** @var \App\Models\User $user */
+                $user = $event->user;
+                Mail::to($user->email)
+                    ->queue(new WelcomeNewUser($user));
+            }
+        );
     }
 
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     */
     public function shouldDiscoverEvents(): bool
     {
         return false;
